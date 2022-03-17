4f230f4917b7468f02f997e478fdae0a54e3aca5
JMM Tech <JMM Tech@DESKTOP-1FP01LF>
2022-03-17 17:24:14 +0500

    UserNameValidationAttribute

---
+++ InternshipTasks/Models/UserNameValidationAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternshipTasks.Models
+{
+    public class UserNameValidationAttribute:ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var _context = (ApplicationDbContext)validationContext.GetService(typeof(ApplicationDbContext));
+            var entity = _context.users.SingleOrDefault(e => e.UserName == value.ToString());
+
+            if (entity != null)
+            {
+                return new ValidationResult(GetErrorMessage(value.ToString()));
+            }
+            return ValidationResult.Success;
+        }
+
+        public string GetErrorMessage(string userName)
+        {
+            return $"UserName {userName} is already in use.";
+        }
+    }
+}
